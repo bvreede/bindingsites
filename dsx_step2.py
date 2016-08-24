43c8c4e0edbda3003201d9ebe722e3d2c1aef184
Barbara Vreede <b.vreede@gmail.com>
2016-08-24 13:15:38 -0400

    first commmit: header

---
+++ dsx_step2.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+'''
+Description: Second step of dsx binding site search: cataloging all identified motifs and
+determining the most frequently found putative binding sites.
+Author: Barbara Vreede
+Contact: b.vreede@gmail.com
+Date: 24 August 2016
+'''
+
+infile = "step1.tsv"
+
